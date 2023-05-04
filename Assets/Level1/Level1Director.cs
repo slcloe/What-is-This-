bd25831e6a9f8ce40ad05cb66be79a83ab91cf23
slcloe <67732600+slcloe@users.noreply.github.com>
2023-05-04 23:26:27 +0900

    set option button.text in level1Scene

--- Assets/Level1/Level1Director.cs
+++ Assets/Level1/Level1Director.cs
@@ -9,16 +9,25 @@ public class Level1Director : MonoBehaviour
 {
     Text learning_word;
     Image learning_img;
-    //Text[] option = new Text[3];
+    Text[] option = new Text[3];
+    string word = TensorFlowLite.SsdSample.detection_text;
 
     void Start()
     {
         learning_word = GameObject.Find("learning_word").GetComponent<Text>();
         learning_img = GameObject.Find("learning_img").GetComponent<Image>();
 
-        string word = TensorFlowLite.SsdSample.detection_text;
-        if (word == null ) { word = string.Empty; }
-        learning_word.text = word;
+        option[0] = GameObject.Find("Button_learn1").GetComponentInChildren<Text>();
+        option[1] = GameObject.Find("Button_learn2").GetComponentInChildren<Text>();
+        option[2] = GameObject.Find("Button_learn3").GetComponentInChildren<Text>();
+
+        //
+        //if (word == null ) { word = string.Empty; }
+        //learning_word.text = word;
+        option[0].text = word;
+        option[1].text = word;
+        option[2].text = word;
+
 
         learning_img.sprite = TensorFlowLite.ScreenCapture.detection_image;
     }
