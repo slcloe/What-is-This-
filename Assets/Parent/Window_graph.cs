e19754c0ebb5a4fa8391689e0b73dba88c696dc2
slcloe <67732600+slcloe@users.noreply.github.com>
2023-05-13 13:49:04 +0900

    add axis separators (graph)

--- Assets/Parent/Window_graph.cs
+++ Assets/Parent/Window_graph.cs
@@ -7,13 +7,19 @@ public class Window_graph : MonoBehaviour
 {
 	[SerializeField] private Sprite circleSprite;
 	private RectTransform graphContainer;
+	private RectTransform labelTemplateX;
+	private RectTransform labelTemplateY;
 
 	private void Awake()
 	{
 		graphContainer = transform.Find("graphContainer").GetComponent<RectTransform>();
+		labelTemplateX = graphContainer.Find("labelTemplateX").GetComponent<RectTransform>();
+		labelTemplateY = graphContainer.Find("labelTemplateY").GetComponent<RectTransform>();
 
-		List<float> valueList = new List<float>() { 5, 100, 56, 45, 30, 22, 33 };
+		List<float> valueList = new List<float>() { 5, 100, 56, 45, 30, 22, 33, 100 };
+		List<float> valueList1 = new List<float>() { 10, 20, 30, 40, 50, 60, 70, 80 };
 		ShowGraph(valueList);
+		ShowGraph(valueList1);
 	}
 
 	private GameObject CreateCircle(Vector2 anchoredPosition)
@@ -33,12 +39,12 @@ public class Window_graph : MonoBehaviour
 	{
 		float graphHeight = graphContainer.sizeDelta.y;
 		float yMaximum = 100f;
-		float xSize = 50f;
+		float xSize = 70f;
 
 		GameObject lastCircleGameObject = null;
 		for (int i = 0; i < valueList.Count; i++)
 		{
-			float xPosition = xSize + i * xSize;
+			float xPosition = (xSize + i * xSize);
 			float yPosition = (valueList[i] / yMaximum) * graphHeight;
 			GameObject circleGameObject = CreateCircle(new Vector2(xPosition, yPosition));
 			if (lastCircleGameObject != null)
@@ -47,6 +53,22 @@ public class Window_graph : MonoBehaviour
 
 			}
 			lastCircleGameObject = circleGameObject;
+
+			RectTransform labelX = Instantiate(labelTemplateX);
+			labelX.SetParent(graphContainer);
+			labelX.gameObject.SetActive(true);
+			labelX.anchoredPosition = new Vector2(xPosition, -20f);
+			labelX.GetComponent<Text>().text=i.ToString();
+		}
+		int separatorCount = 10;
+		for (int i = 0; i <= separatorCount; i++)
+		{
+			RectTransform labelY = Instantiate(labelTemplateY);
+			labelY.SetParent(graphContainer, false);
+			labelY.gameObject.SetActive(true);
+			float normalizedValue = i * 1f / separatorCount;
+			labelY.anchoredPosition = new Vector2(-7f, normalizedValue * graphHeight);
+			labelY.GetComponent<Text>().text = Mathf.RoundToInt(normalizedValue * yMaximum).ToString();
 		}
 	}
 
