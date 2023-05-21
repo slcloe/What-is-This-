1d26e39b50c22dd717c78de244fb60bdd86512a1
Boeun <boeun0904@naver.com>
2023-05-22 01:19:08 +0900

    Feat: mainscene & homescene background music

--- Assets/LogIn/MainDirector.cs
+++ Assets/LogIn/MainDirector.cs
@@ -8,13 +8,20 @@ public class MainDirector : MonoBehaviour
 {
     Button btLogin;
     Button btSignUp;
+    GameObject star;
     void Start()
     {
         btLogin = GameObject.Find("ButtonLogin").GetComponent<Button>();
         btSignUp = GameObject.Find("ButtonSignUp").GetComponent<Button>();
+        star = GameObject.Find("Star");
 
         btLogin.onClick.AddListener(GotoLogin);
         btSignUp.onClick.AddListener(GotoSignUp);
+    }
+
+    void Update()
+    {
+        star.transform.Rotate(0, 0, 30 * Time.deltaTime);
     }
 
     void GotoLogin()
